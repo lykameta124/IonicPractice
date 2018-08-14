5d543b62eba29daccb33ec9b5952e96a594dab96
knedio <knedio.124@gmail.com>
2018-08-14 18:19:11 +0800

    New

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { AlertController } from 'ionic-angular';
+import { AboutPage } from '../about/about';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
-export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+export class HomePage {
+  public users = [
+    { id: 1, username: 'Karl', password: '123' },
+    { id: 2, username: 'Naio', password: '123' },
+    { id: 3, username: 'Edio', password: '123' }
+  ];
+  name: string = '';
+  constructor(public navCtrl: NavController,public alertCtrl: AlertController) {
 
   }
-
+  login(){
+    let found = this.users.some(function (a) {
+      return a.username = this.name;
+    });
+    if(found){
+      this.navCtrl.push(AboutPage);
+    }
+  }
 }
